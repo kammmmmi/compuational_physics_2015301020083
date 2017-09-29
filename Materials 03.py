ddc809307f1fb488b4d761ca1e658a302558967c
kammmmmi <31853237+kammmmmi@users.noreply.github.com>
2017-09-30 01:50:18 +0800

    Update Materials 03.py

--- Materials 03.py
+++ Materials 03.py
@@ -1,31 +1,18 @@
-import numpy as a 
-
-import matplotlib.pyplot as b
-
+import numpy as np 
+import matplotlib.pyplot as pl
 v = [0.]  
 t = [0.] 
 g = 9.8
 dt = 0.1
 end_time = 10
-
 for i in range(int(end_time / dt)):
-
 	k = v[i] - g * dt
-
 	v.append(k)
-
 	t.append(dt * (i + 1))     
-
 	print t[-1], v[-1]
-
-b.figure(figsize=(8,6)) 
-
-b.plot(t,x,label="x(t)",color="red",linewidth=1)
-
-b.xlabel("t(s)") 
-
-b.ylabel("v(m/s)") 
-
-b.legend()  
-
-b.show() 
+pl.figure(figsize=(8,6)) 
+pl.plot(t,x,label="x(t)",color="red",linewidth=1)
+pl.xlabel("t(s)") 
+pl.ylabel("v(m/s)") 
+pl.legend()  
+pl.show() 
